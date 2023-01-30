ab3a039f01077c6f47d61107242248a8805c9cfc
sreplas <61392290+sreplas@users.noreply.github.com>
2023-01-30 15:29:22 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
+STATIC_ROOT = ( BASE_DIR.as_posix() + '/static' )
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
